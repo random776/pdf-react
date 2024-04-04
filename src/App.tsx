19cbaf596bd78ab9650a18c3b7f87b6c6c62a1c9
random776 <125573400+random776@users.noreply.github.com>
2024-04-04 19:39:33 +0900

    successfully extract japanese word from pdf

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { Document, Page, pdfjs } from "react-pdf"; // Make sure to import pdfjs
 import secondPDF from "/chibutsu_nyumon.pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
-import parse from "html-react-parser";
-import { createWorker } from "tesseract.js";
 
 // Set the worker source path for pdfjs
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
@@ -12,8 +10,9 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/b
 export default function App() {
   const [numPages, setNumPages] = useState<number>(0);
   const [pageNumber, setPageNumber] = useState<number>(1);
-  const [string, setString] = useState("");
-  const [ocr, setOcr] = useState("");
+  const [result, setResult] = useState<string[]>([]); // 新しいStateを定義
+  const resultList: string[] = []; // pdfから抜き取った情報を、ここから抜いていく。
+
   const options = useMemo(
     () => ({
       cMapUrl: `https://unpkg.com/pdfjs-dist@${pdfjs.version}/cmaps/`,
@@ -22,12 +21,7 @@ export default function App() {
     []
   );
 
-  // render the first page of the pdf file using `PageNumber` property of `Page` when loaded pdf file.
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    setNumPages(numPages);
-  }
-
-  // move the page that is rendered.
+  // ページ移動
   const goToPrevPage = (): void => {
     if (pageNumber > 1) {
       setPageNumber(pageNumber - 1);
@@ -40,65 +34,34 @@ export default function App() {
     }
   };
 
-  // try to extract strings from pdf file
-  async function extractTextFromPDF(pdfUrl: any, options: any) {
-    const loadingTask = pdfjs.getDocument(pdfUrl);
-    const pdf = await loadingTask.promise;
+  // pdfから文字を抜き出す非同期関数
+  async function extractTextFromPDF(pdf: {
+    getPage: (arg0: any) => any;
+    numPages: any;
+  }) {
+    const getPageText = async (pageNum: number) => {
+      const page = await pdf.getPage(pageNum);
+      const textContent = await page.getTextContent();
+      const renderedTextContent = textContent.items
+        .map((item: { str: any }) => item.str)
+        .join("");
+      resultList.push(renderedTextContent);
+      setResult(resultList);
+      console.log(result);
+    };
+
     const numPages = pdf.numPages;
-    let fullText = "";
 
     for (let i = 1; i <= numPages; i++) {
-      const page = await pdf.getPage(i);
-      const textContent = await page.getTextContent(options);
-      console.log(textContent.items);
-      let text = "";
-      for (let textItem of textContent.items) {
-        if ("str" in textItem) {
-          text += textItem?.str + "\n";
-        }
-      }
-      fullText += text + "\n";
+      await getPageText(i);
     }
-
-    return fullText;
   }
 
-  // extract strings from `photo.jpg` in `public` using `tesseract.js`
-  async function ocrText(path: string) {
-    const worker = await createWorker("jpn");
-    const ret = await worker.recognize(path);
-    await worker.terminate();
-    const strippedText = ret.data.text.replace(/ /g, "");
-    console.log(strippedText);
-    return strippedText;
+  async function onDocumentLoadSuccess(pdf: { numPages: any; getPage: any }) {
+    const numPages = pdf.numPages;
+    setNumPages(numPages); // Extract text from PDF
+    await extractTextFromPDF(pdf);
   }
-
-  // usage of extractTextFromPDF
-  const pdfUrl = secondPDF;
-
-  extractTextFromPDF(pdfUrl, options)
-    .then((text) => {
-      setString(text);
-    })
-    .catch((error) => {
-      console.error("Error extracting text from PDF:", error);
-    });
-
-  const renderedText = string; //.replace(/\n/g, "<br>");
-
-  // usage of ocrText
-
-  const path = "/photo.jpg";
-
-  ocrText(path)
-    .then((text) => {
-      setOcr(text);
-    })
-    .catch((error) => {
-      console.error("Error OCR:", error);
-    });
-
-  const renderedOCR = ocr.replace(/\n/g, "<br>");
 
   return (
     <div>
@@ -108,10 +71,6 @@ export default function App() {
         <ul>
           <li>
             <a href="https://react-pdf.org">react-pdf</a>
-          </li>
-          <p>reactむけのPDF.jsのようなものらしい。</p>
-          <li>
-            <a href="https://tesseract.projectnaptha.com">tesseract.js</a>
           </li>
         </ul>
         <h3>React-pdfを用いてPDFファイルを表示する。</h3>
@@ -134,28 +93,12 @@ export default function App() {
         </Document>
       </div>
       <div className="blockb">
-        <h3>方法論その1: react-pdfを用いて文字情報をそのまま持ってくる。</h3>
+        <h3>方法: react-pdfを用いて文字情報をそのまま持ってくる。</h3>
         <p>
-          日本語ファイルがうまく表示されないことがあって苦戦（うまくいくこともある）。
+          日本語ファイルがうまく表示されないことがあって苦戦したが、うまくいった。
         </p>
         <hr />
-        <p>{parse(renderedText)}</p>
-        <hr />
-        <h3>
-          方法論その2:
-          tesseract.jsなどを用いてOCRによって写真から文字を読み取る。
-        </h3>
-        <p>
-          これはPDFそのものから持ってきたものではなく、PDFのスクリーンショットを読み込ませている。
-        </p>
-        <p>
-          スクリーンショットなど画像ファイルから文字を読み取る仕様ならこれでも良い。
-        </p>
-        <p>
-          PDFファイルから読み取るなら、PDFを写真として読み取る方法が文字起こしには必要。
-        </p>
-        <hr />
-        <p>{parse(renderedOCR)}</p>
+        <p>{result[pageNumber - 1]}</p>
         <hr />
       </div>
     </div>
